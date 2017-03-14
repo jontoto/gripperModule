76dd1c6d51d7155c0d1e190be47ac44e6fbd85b8
Jon Toto <jontoto@umich.edu>
2017-03-14 16:49:01 -0400

    vhajfg

--- gripper_rave_module.cpp
+++ gripper_rave_module.cpp
@@ -2,6 +2,7 @@
 #include <openrave/plugin.h>
 #include <boost/bind.hpp>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 using namespace OpenRAVE;
@@ -10,54 +11,63 @@ namespace robotiq_gripper{
 #define M1 0.5
 #define M2 0.9
 
-    class GripperFinger{
+    static inline double deg2rad(double angle){
+        return angle *M_PI / 180.0}
+    }
+
+    class GripperModule : public ModuleBase
+    {
+
     private:
-        vector<int> joints(3,0);
-        int commandPos = 0;
-
-
-        int m3(){
-            return -55 +(43+55)/(255 - commandPos);
-        }
-
-    public:
-        vector<int>& get_joint_values() const{
-            return joints;
-        }
-
+        vector<double> joints(9);
+        vector<int> commandPos(3);
+        RobotBasePtr robot;
+        
         void no_obj_update_joints(){
-            if(commandPos <= 110){
-                joints[0] = M1*commandPos;
-                joints[1] = 0;
-                joints[2] = -M1*commandPos;
-            }else if(commandPos <= 140){
-                joints[0] = M1*commandPos;
-                joints[1] = 0;
-                jointsp[2] = -55.0 ;
-            }else if(commandPos <= 240){
-                joints[0] = 70.0;
-                joints[1] = M2*(commandPos - 140);
-                joints[2] = -55.0;
-            }else{
-                joints[0] = 70.0;
-                joints[1] = 90.0;
-                joints[2] = -55.0;
+            for(int i = 0; i  < 3; i++){
+                if(commandPos <= 110){
+                    joints[i*3 +0] = deg2rad(M1*commandPos[i]);
+                    joints[i*3 +1] = 0;
+                    joints[i*3 +2] = deg2rad(-M1*commandPos[i]);
+                }else if(commandPos <= 140){
+                    joints[i*3 +0] = deg2rad(M1*commandPos[i]);
+                    joints[i*3 +1] = 0;
+                    joints[i*3 +2] = deg2rad(-55.0);
+                }else if(commandPos <= 240){
+                    joints[i*3 +0] = deg2rad(70.0);
+                    joints[i*3 +1] = deg2rad(M2*(commandPos[i] - 140));
+                    joints[i*3 +2] = deg2rad(-55.0);
+                }else{
+                    joints[i*3 +0] = deg2rad(70.0);
+                    joints[i*3 +1] = deg2rad(90.0);
+                    joints[i*3 +2] = deg2rad(-55.0);
+                }
+            
             }
+
+            robot.SetDOFValues(joints);
 
             return;
         }
 
-        void update_commandPos(int g){
-            commandPos = g;
+    public:
+        GripperModule(EnvironmentBasePtr penv) : ModuleBase(penv){
+            _description = "gives joint information for Robotiq s_model gripper (no object handling so far)";
+            RegisterCommand("getJoints", boost::bind(GripperModule:: getJoints, this), "returns vector of joint values");
+            RegisterCommand("update_commandPos", boost::bind(GripperModule::update_commandPos, this, _1), "updates the vector of command values for the fingers");
+            RegisterCommand("getEffectorTransform")
+            robot = GetEnv()->GetRobots()[0];
         }
-    };
 
-    class GripperModule : public ModuleBase
-    {
-    private:
 
-        _description = "gives joint information for Robotiq s_model gripper (no object handling so far)";
-        RegisterCommand("getJoints", )
+        vector<vector<int> >&  getJoints() const{
+            return joints;
+        }
 
+        void update_commandPos(const vector<int> g_new){
+            commandPos = g_new;
+
+            no_obj_update_joints();
+        }
     };
 }
